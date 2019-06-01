efb91d16e05279fb619f8ef64c8b5018134a19d8
daisuke.kitagawa <daisuke.nagi@gmail.com>
2019-06-01 01:11:05 +0000

    syusei

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -41,6 +41,7 @@ class TasksController extends Controller
     {
         $this->validate($request, [
             'content' => 'required|max:191',
+            'status' => 'required|max:10',
         ]);
 
         $request->user()->tasks()->create([
